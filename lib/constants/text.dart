b0f3be0840658021ee5f751d5e876f9c753eaa26
Harmanjit Singh <harmanjit140500@gmail.com>
2021-03-16 15:50:08 +0530

    text++

--- lib/constants/text.dart
+++ lib/constants/text.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class RegularText extends StatelessWidget {
-  const RegularText(this.text, this.color, this.size);
   final String text;
   final Color color;
   final double size;
+
+  const RegularText({Key key, this.text, this.color, this.size})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Text(
