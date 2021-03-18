4a6c26e9e941aa632d34927fa431880e3753e2d3
Harmanjit Singh <harmanjit140500@gmail.com>
2021-03-18 19:30:27 +0530

    login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,3 +1,5 @@
+import 'package:Huddle/constants/text.dart';
+import 'package:Huddle/screens/log_holder.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -6,6 +8,8 @@ class ButtonLogin extends GetxController {
 }
 
 class LoginSreen extends StatelessWidget {
+  final TextEditingController username = new TextEditingController();
+  final TextEditingController password = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,8 +24,77 @@ class LoginSreen extends StatelessWidget {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisSize: MainAxisSize.max,
+              children: [
+                IconButton(
+                    icon: Icon(Icons.arrow_back_outlined),
+                    onPressed: () {
+                      Get.off(() => Login());
+                    })
+              ],
             ),
-          )
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+            child: BoldText(
+              text: "Let's sign you in.",
+              size: 37,
+              color: Colors.black,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 0, 20, 10),
+            child: RegularText(
+              text: "Welcome back.\nYou've been missed!",
+              size: 28,
+              color: Colors.black,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 60, 20, 0),
+            child: TextField(
+              controller: username,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: "Username",
+                prefixIcon: Icon(Icons.account_circle),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+            child: TextField(
+              controller: password,
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              obscuringCharacter: "*",
+              decoration: InputDecoration(
+                labelText: "Password",
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
+          ),
+           Container(
+            margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+            child: TextField(
+              controller: password,
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              obscuringCharacter: "*",
+              decoration: InputDecoration(
+                labelText: "Password",
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
+          ),
         ],
       )),
     );
