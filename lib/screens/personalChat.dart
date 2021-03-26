67f103acd24cb1c59aad914d30655758c8e672fb
Harmanjit Singh <harmanjit140500@gmail.com>
2021-03-26 18:08:28 +0530

    I am cutest

--- lib/screens/personalChat.dart
+++ lib/screens/personalChat.dart
@@ -1,5 +1,8 @@
+import 'package:Huddle/constants/text.dart';
 import 'package:Huddle/models/chatModel.dart';
+import 'package:Huddle/screens/holder.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MyChatScreen extends StatefulWidget {
   final ChatData obj;
@@ -13,26 +16,80 @@ class _MyChatScreenState extends State<MyChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            height: 100,
-            color: Colors.green,
-          ),
-          Expanded(
-              child: ListView(
-            children: [
-              Text("hi"),
-            ],
-          )),
-          Container(
-            height: 100,
-            color: Colors.amber,
-          ),
-        ],
+      body: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 80,
+              color: Colors.blue[400],
+              padding: EdgeInsets.fromLTRB(15, 10, 20, 10),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconButton(
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        Get.off(() => AllChats());
+                      }),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  CircleAvatar(
+                    backgroundImage:
+                        NetworkImage(widget.obj.userImage.toString()),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  BoldText(
+                      text: widget.obj.userName.toString(),
+                      color: Colors.white,
+                      size: 16),
+                ],
+              ),
+            ),
+            Expanded(
+                child: ListView(
+              children: [
+                Text("hi"),
+              ],
+            )),
+            Container(
+              height: 80,
+              padding: EdgeInsets.fromLTRB(10, 10, 20, 10),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        labelText: "Message",
+                        prefixIcon: Icon(Icons.message_outlined),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.send),
+                    onPressed: () {},
+                    color: Colors.blue,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
