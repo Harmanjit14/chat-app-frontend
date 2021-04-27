20df282b7adc61a077f07e1c33142e2e8e43f873
Harmanjit Singh <harmanjit140500@gmail.com>
2021-04-27 19:15:49 +0530

    find

--- lib/screens/newppl.dart
+++ lib/screens/newppl.dart
@@ -1,3 +1,4 @@
+import 'package:Huddle/constants/text.dart';
 import 'package:flutter/material.dart';
 
 class FindPeople extends StatefulWidget {
@@ -8,8 +9,27 @@ class FindPeople extends StatefulWidget {
 class _FindPeopleState extends State<FindPeople> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return SafeArea(
+      child: Container(
+        child: ListView(
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 30, 0, 0),
+              child: ShadowBoldText(
+                text: "Find Friends",
+                color: Colors.black,
+                size: 40,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
+              child: TextField(
+                
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
