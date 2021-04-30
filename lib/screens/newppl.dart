325883ba50c6de7541b02c69627188be64376b9c
Harmanjit Singh <harmanjit140500@gmail.com>
2021-04-30 14:11:35 +0530

    setup

--- lib/screens/newppl.dart
+++ lib/screens/newppl.dart
@@ -1,5 +1,7 @@
 import 'package:Huddle/constants/text.dart';
+import 'package:Huddle/models/nearMe.dart';
 import 'package:Huddle/server/getLocation.dart';
+import 'package:Huddle/server/getNear.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -23,6 +25,20 @@ class _FindPeopleState extends State<FindPeople> {
   void dispose() {
     controller.dispose();
     super.dispose();
+  }
+
+  Widget nearMeBox(NearMe obj) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: (obj.isFemal) ? Colors.pink[100] : Colors.blue[100],
+          boxShadow: [BoxShadow(color: Colors.grey)]),
+      height: 80,
+      width: 100,
+      child: SizedBox.expand(),
+    );
   }
 
   @override
@@ -54,6 +70,9 @@ class _FindPeopleState extends State<FindPeople> {
               ),
             ),
             Container(
+              child: BoldText(text: "Near me", color: Colors.black, size: 16),
+            ),
+            Container(
               margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
               child: Row(
                 mainAxisSize: MainAxisSize.max,
@@ -68,7 +87,7 @@ class _FindPeopleState extends State<FindPeople> {
                   SizedBox(width: 6),
                   RegularText(
                     text:
-                        "${currentLoc.street.toString()} ${currentLoc.dist.toString()} ${currentLoc.state.toString()}",
+                        "${currentLoc.street.toString()}, ${currentLoc.dist.toString()}, ${currentLoc.state.toString()}",
                     color: Colors.grey[700],
                     size: 15,
                   ),
@@ -79,26 +98,10 @@ class _FindPeopleState extends State<FindPeople> {
               height: 120,
               margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
               child: ListView.builder(
-                  itemCount: 29,
+                  itemCount: nearMe.length,
                   scrollDirection: Axis.horizontal,
                   itemBuilder: (context, index) {
-                    return Container(
-                      margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey
-                          )
-                        ]
-                      ),
-                      height: 80,
-                      width: 100,
-                      child: SizedBox.expand(
-                        
-                      ),
-                    );
+                    return nearMeBox(nearMe[index]);
                   }),
             ),
           ],
